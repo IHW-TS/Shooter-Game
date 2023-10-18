f373db6820e6e5dd4f59033c100176fba8977cba
IHW-TS <78849961+IHW-TS@users.noreply.github.com>
2023-10-18 18:52:58 +0200

    vf

--- js/main.js
+++ js/main.js
@@ -1,37 +1,42 @@
 let playerLives = 10;
 let enemiesKilled = 0;
 let money = 0;
+let spawnEnemyInterval;
 
 function gameLoop() {
     if (!isPaused) {
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-        // Dessinez le joueur
+        ctx.clearRect(0, 0, canvas.width, canvas.height); 
+        
         player.draw();
+        player.move();
+        
+        if (keys.Space) {
+            shoot();
+            keys.Space = false;  
+        }
 
-        // Mettez à jour et dessinez les projectiles
         updateProjectiles();
         projectiles.forEach(projectile => {
             ctx.fillStyle = projectile.color;
             ctx.fillRect(projectile.x, projectile.y, projectile.width, projectile.height);
         });
 
-        // Mettez à jour et dessinez les ennemis
         updateEnemies();
         enemies.forEach(enemy => {
             ctx.fillStyle = enemy.color;
             ctx.fillRect(enemy.x, enemy.y, enemy.width, enemy.height);
         });
 
-        // Dessinez le tableau de bord (HUD)
         drawHUD();
+    }
+
+    if (playerLives <= 0) {
+        displayGameOver();
+        return; 
     }
 
     requestAnimationFrame(gameLoop);
 }
-
-// Logique pour faire apparaître les ennemis à intervalles réguliers
-setInterval(spawnEnemy, 1000);
 
 function drawHUD() {
     ctx.fillStyle = 'black';
@@ -41,4 +46,24 @@ function drawHUD() {
     ctx.fillText('Argent: $' + money, 10, 60);
 }
 
+function displayGameOver() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.fillStyle = 'black';
+    ctx.font = '40px Arial';
+    ctx.fillText('Game Over', canvas.width / 2 - 100, canvas.height / 2);
+}
+
+function startGame() {
+    spawnEnemyInterval = setInterval(spawnEnemy, 1000);  // Démarrage de l'intervalle pour faire apparaître des ennemis
+}
+
+function togglePause() {
+    isPaused = !isPaused;
+    if (isPaused) {
+        clearInterval(spawnEnemyInterval);  // Annulation de l'intervalle lorsque le jeu est mis en pause
+    } else {
+        spawnEnemyInterval = setInterval(spawnEnemy, 1000);  // Redémarrage de l'intervalle lorsque le jeu est repris
+    }
+}
+startGame();
 gameLoop();
--- js/player.js
+++ js/player.js
@@ -1,3 +1,46 @@
+const keys = {
+    ArrowUp: false,
+    ArrowDown: false,
+    ArrowLeft: false,
+    ArrowRight: false,
+    Space: false,
+    p: false,
+    b: false
+};
+
+window.addEventListener('keydown', event => {
+    console.log(event.key);  // Ajoutez cette ligne
+    if (keys.hasOwnProperty(event.key)) {
+        keys[event.key] = true;
+        if (event.key === 'p') togglePause();
+        if (event.key === 'b') toggleShop();
+        if (event.key === 'Space') {
+            console.log('Space key pressed');  // Ajoutez cette ligne
+            shoot();
+        }
+    }
+});
+
+function shoot() {
+    console.log('shoot function called');  // Ajoutez cette ligne
+    const projectile = {
+        x: player.x + player.width / 2 - 2.5,
+        y: player.y,
+        width: 5,
+        height: 10,
+        speed: 10,
+        color: 'green'
+    };
+    projectiles.push(projectile);
+}
+
+
+window.addEventListener('keyup', event => {
+    if (keys.hasOwnProperty(event.key)) {
+        keys[event.key] = false;
+    }
+});
+
 const player = {
     x: canvas.width / 2 - 25,
     y: canvas.height - 60,
@@ -8,31 +51,15 @@ const player = {
         ctx.fillStyle = this.color;
         ctx.fillRect(this.x, this.y, this.width, this.height);
     },
-    move: function(x, y) {
-        this.x += x;
-        this.y += y;
+    move: function() {
+        if (keys.ArrowUp && this.y > 0) this.y -= 10;
+        if (keys.ArrowDown && this.y < canvas.height - this.height) this.y += 10;
+        if (keys.ArrowLeft && this.x > 0) this.x -= 10;
+        if (keys.ArrowRight && this.x < canvas.width - this.width) this.x += 10;
+        if (keys.Space) shoot();
     }
 };
 
-window.addEventListener('keydown', function(event) {
-    switch(event.key) {
-        case 'ArrowUp':
-            player.move(0, -30);
-            break;
-        case 'ArrowDown':
-            player.move(0, 30);
-            break;
-        case 'ArrowLeft':
-            player.move(-30, 0);
-            break;
-        case 'ArrowRight':
-            player.move(30, 0);
-            break;
-        case ' ':
-            shoot();
-            break;
-        case 'p':
-            togglePause();
-            break;
-    }
-});
+function toggleShop() {
+    isPaused = !isPaused;
+}
